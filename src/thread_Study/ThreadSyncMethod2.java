f1e097a54bdcccdff02996abe010c4b8b2d34335
gusrms1001 <gusrms1001@naver.com>
2021-05-14 18:33:50 +0900

    ThreadTest

--- src/thread_Study/ThreadSyncMethod2.java
+++ src/thread_Study/ThreadSyncMethod2.java
@@ -1,4 +1,81 @@
 package thread_Study;
 
-public class ThreadSyncMethod2 {
+
+class Calculator2 {
+    int opCnt=0 ;
+    int mytot = 10000 ;
+    int youtot = 10000 ;
+
+    public int add(int n1, int n2) {
+        synchronized(this) {
+            opCnt++;  // 동기화 필요
+            mytot += (n1+n2) ;  // you 가 my 에게 입금
+            youtot -= (n1+n2) ;
+            System.out.println("my 에게 입금: mytot= "+mytot+ " , youtot= "+youtot);
+        }
+        return n1+n2;
+    }
+    public int min(int n1, int n2) {
+        synchronized(this) {
+            opCnt++; // 동기화 필요
+            mytot -= (n1+n2) ;  // my 가 출금 you 에게 입금
+            youtot += (n1+n2) ;
+            System.out.println("my 가 출금: mytot= "+mytot+ " , youtot= "+youtot);
+        }
+        return n1-n2;
+    }
+    public void showOpCnt() {
+        System.out.println("총 연산 횟수: "+opCnt);
+    }
+}
+
+class AddThread2 extends Thread {
+    Calculator2 cal;
+
+    public AddThread2(Calculator2 cal) { this.cal=cal; }
+
+    public void run() {
+        System.out.println("10+20="+cal.add(10, 20));
+        System.out.println("20+40="+cal.add(20, 40));
+    }
+}
+
+class MinThread2 extends Thread {
+    Calculator2 cal;
+
+    public MinThread2(Calculator2 cal) { this.cal=cal; }
+
+    public void run() {
+        System.out.println("30-20="+cal.min(30, 20));
+        System.out.println("40-30="+cal.min(40, 30));
+    }
+}
+
+class ThreadSyncMethod2 {
+    public static void main(String[] args) {
+        Calculator2 cal=new Calculator2();
+        AddThread2 at=new AddThread2(cal);
+        MinThread2 mt=new MinThread2(cal);
+
+        AddThread2 at1=new AddThread2(cal);
+        AddThread2 at2=new AddThread2(cal);
+        MinThread2 mt2=new MinThread2(cal);
+
+        at.start();
+        mt.start();
+        at1.start();
+        mt2.start();
+        at2.start();
+
+        try {
+            at.join();
+            at1.join();
+            at2.join();
+            mt.join();
+            mt2.join();
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        cal.showOpCnt();
+    }
 }
--- src/thread_Study/WaitNotifyExample.java
+++ src/thread_Study/WaitNotifyExample.java
@@ -1,4 +1,70 @@
 package thread_Study;
 
-public class WaitNotifyExample {
+
+
+class ThreadA1 extends Thread {
+    private WorkObject workObject;
+
+    public ThreadA1(WorkObject workObject) {
+        this.workObject = workObject;
+    }
+
+    @Override
+    public void run() {
+        for(int i=0; i<10; i++) {
+            workObject.methodA();
+            System.out.print(" ai : " +i) ;
+            System.out.println() ;
+        }
+    }
 }
+
+class ThreadB1 extends Thread {
+    private WorkObject workObject;
+
+    public ThreadB1(WorkObject workObject) {
+        this.workObject = workObject;
+    }
+
+    @Override
+    public void run() {
+        for(int i=0; i<10; i++) {
+            System.out.print(" bi : " +i) ;
+            System.out.println() ;
+            workObject.methodB();
+        }
+    }
+}
+class WorkObject {
+    public synchronized void methodA() {
+        System.out.print("ThreadA의 methodA() 작업 실행");
+        notify(); // 작업을 마친 후 Blocked 를 깨우고
+        try {
+            wait(); // 자신은 wait 로
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public synchronized void methodB() {
+        System.out.print("ThreadB의 methodB() 작업 실행");
+        notify();
+        try {
+            wait();
+        } catch (InterruptedException e) {
+        }
+    }
+}
+
+public class WaitNotifyExample {
+    public static void main(String[] args) {
+        WorkObject sharedObject = new WorkObject();
+
+        ThreadA1 threadA = new ThreadA1(sharedObject);
+        ThreadB1 threadB = new ThreadB1(sharedObject);
+
+        threadA.start();
+        threadB.start();
+
+    }
+}
+
